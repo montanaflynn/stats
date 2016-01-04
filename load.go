2bf4b2115f487afa41b7a99389d8745324a2d0b2
brydavis <bryan@stax.io>
2016-01-03 16:02:13 -0800

    auto commit

---
+++ load.go
@@ -0,0 +1,78 @@
+package stats
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// LoadRawData parses and converts a slice of mixed data types to floats
+func LoadRawData(raw interface{}) (f Float64Data) {
+	var r []interface{}
+	var s Float64Data
+
+	switch t := raw.(type) {
+	case []interface{}:
+		r = t
+	case []uint:
+		for _, v := range t {
+			s = append(s, float64(v))
+		}
+		return s
+	case []float64:
+		return Float64Data(t)
+	case []int:
+		for _, v := range t {
+			s = append(s, float64(v))
+		}
+		return s
+	case []string:
+		for _, v := range t {
+			r = append(r, v)
+		}
+	case map[int]int:
+		for i := 0; i < len(t); i++ {
+			s = append(s, float64(t[i]))
+		}
+		return s
+	case map[int]string:
+		for i := 0; i < len(t); i++ {
+			r = append(r, t[i])
+		}
+	case map[int]uint:
+		for i := 0; i < len(t); i++ {
+			s = append(s, float64(t[i]))
+		}
+		return s
+	case map[int]float64:
+		for i := 0; i < len(t); i++ {
+			s = append(s, t[i])
+		}
+		return s
+	}
+
+	for _, v := range r {
+		switch t := v.(type) {
+		case int:
+			a := float64(t)
+			f = append(f, a)
+		case uint:
+			f = append(f, float64(t))
+		case float64:
+			f = append(f, t)
+		case string:
+			fl, err := strconv.ParseFloat(t, 64)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				f = append(f, fl)
+			}
+		case bool:
+			if t == true {
+				f = append(f, 1.0)
+			} else {
+				f = append(f, 0.0)
+			}
+		}
+	}
+	return f
+}
