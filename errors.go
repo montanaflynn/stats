2039aba653f23b8a90c7fdbebd4341efca493a1e
Montana Flynn <montana949@gmail.com>
2019-01-28 13:48:15 +0700

    Use a single var block for exported errors

--- errors.go
+++ errors.go
@@ -1,6 +1,3 @@
-// Package stats error values, all exported APIs
-// should use these errors as returned values
-// https://github.com/golang/go/wiki/Errors#naming
 package stats
 
 type statsError struct {
@@ -15,26 +12,24 @@ func (s statsError) String() string {
 	return s.err
 }
 
-// ErrEmptyInput Input must not be empty
-var ErrEmptyInput = statsError{"Input must not be empty."}
-
-// ErrNaN Not a number
-var ErrNaN = statsError{"Not a number."}
-
-// ErrNegative Must not contain negative values
-var ErrNegative = statsError{"Must not contain negative values."}
-
-// ErrZero Must not contain zero values
-var ErrZero = statsError{"Must not contain zero values."}
-
-// ErrBounds Input is outside of range
-var ErrBounds = statsError{"Input is outside of range."}
-
-// ErrSize Must be the same length
-var ErrSize = statsError{"Must be the same length."}
-
-// ErrInfValue Value is infinite
-var ErrInfValue = statsError{"Value is infinite."}
-
-// ErrYCoord Y Value must be greater than zero
-var ErrYCoord = statsError{"Y Value must be greater than zero."}
+// These are the package-wide error values.
+// All error identification should use these values.
+// https://github.com/golang/go/wiki/Errors#naming
+var (
+	// ErrEmptyInput Input must not be empty
+	ErrEmptyInput = statsError{"Input must not be empty."}
+	// ErrNaN Not a number
+	ErrNaN = statsError{"Not a number."}
+	// ErrNegative Must not contain negative values
+	ErrNegative = statsError{"Must not contain negative values."}
+	// ErrZero Must not contain zero values
+	ErrZero = statsError{"Must not contain zero values."}
+	// ErrBounds Input is outside of range
+	ErrBounds = statsError{"Input is outside of range."}
+	// ErrSize Must be the same length
+	ErrSize = statsError{"Must be the same length."}
+	// ErrInfValue Value is infinite
+	ErrInfValue = statsError{"Value is infinite."}
+	// ErrYCoord Y Value must be greater than zero
+	ErrYCoord = statsError{"Y Value must be greater than zero."}
+)
