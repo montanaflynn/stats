ce31985b3920588edaffc28439e43c4fa505ecc6
xiaobogaga <m18202787110@gmail.com>
2019-08-16 00:35:33 -0500

    Add StableSample function (#48)

--- sample_test.go
+++ sample_test.go
@@ -7,12 +7,12 @@ import (
 func TestSample(t *testing.T) {
 	_, err := Sample([]float64{}, 10, false)
 	if err == nil {
-		t.Errorf("Returned an error")
+		t.Errorf("should return an error")
 	}
 
-	_, err2 := Sample([]float64{0.1, 0.2}, 10, false)
-	if err2 == nil {
-		t.Errorf("Returned an error")
+	_, err = Sample([]float64{0.1, 0.2}, 10, false)
+	if err == nil {
+		t.Errorf("should return an error")
 	}
 }
 
@@ -35,5 +35,33 @@ func TestSampleWithReplacement(t *testing.T) {
 	result, _ := Sample(arr, numsamples, true)
 	if len(result) != numsamples {
 		t.Errorf("%v != %v", len(result), numsamples)
+	}
+}
+
+func TestStableSample(t *testing.T) {
+	_, err := StableSample(Float64Data{}, 10)
+	if err != EmptyInputErr {
+		t.Errorf("should return EmptyInputError when sampling an empty data")
+	}
+	_, err = StableSample(Float64Data{1.0, 2.0}, 10)
+	if err != BoundsErr {
+		t.Errorf("should return BoundsErr when sampling size exceeds the maximum element size of data")
+	}
+	arr := []float64{1.0, 3.0, 2.0, -1.0, 5.0}
+	locations := map[float64]int{
+		1.0:  0,
+		3.0:  1,
+		2.0:  2,
+		-1.0: 3,
+		5.0:  4,
+	}
+	ret, _ := StableSample(arr, 3)
+	if len(ret) != 3 {
+		t.Errorf("returned wrong sample size")
+	}
+	for i := 1; i < 3; i++ {
+		if locations[ret[i]] < locations[ret[i-1]] {
+			t.Errorf("doesn't keep order")
+		}
 	}
 }
