182d06e7fd5e49105366aaff6df144ab4d902be9
Montana Flynn <montana949@gmail.com>
2026-04-12 09:21:44 -0500

    feat: add Spearman rank correlation function (#98)
    Closes #69

--- correlation_test.go
+++ correlation_test.go
@@ -56,6 +56,103 @@ func TestCorrelation(t *testing.T) {
 	}
 }
 
+func ExampleSpearman() {
+	s1 := []float64{1, 2, 3, 4, 5}
+	s2 := []float64{5, 6, 7, 8, 7}
+	a, _ := stats.Spearman(s1, s2)
+	rounded, _ := stats.Round(a, 5)
+	fmt.Println(rounded)
+	// Output: 0.82078
+}
+
+func TestSpearman(t *testing.T) {
+	testCases := []struct {
+		name   string
+		data1  []float64
+		data2  []float64
+		output float64
+		err    error
+	}{
+		// Error cases
+		{"Empty Slice Error", []float64{}, []float64{}, math.NaN(), stats.EmptyInputErr},
+		{"Different Length Error", []float64{1, 2, 3, 4, 5}, []float64{10, -51.2, 8}, math.NaN(), stats.SizeErr},
+
+		// Perfect correlation
+		{"Perfect Positive", []float64{1, 2, 3, 4, 5}, []float64{1, 2, 3, 4, 5}, 1.0, nil},
+		{"Perfect Negative", []float64{1, 2, 3, 4, 5}, []float64{5, 4, 3, 2, 1}, -1.0, nil},
+
+		// Verified against SciPy spearmanr and R cor.test(method="spearman")
+		{
+			"Basic with tie in y (SciPy/R)",
+			[]float64{1, 2, 3, 4, 5},
+			[]float64{5, 6, 7, 8, 7},
+			0.8207826816681233,
+			nil,
+		},
+		{
+			"Negative with tie (SciPy mstats)",
+			[]float64{5.05, 6.75, 3.21, 2.66},
+			[]float64{1.65, 2.64, 2.64, 6.95},
+			-0.632455532033676,
+			nil,
+		},
+		{
+			"15 elements with ties in both (SciPy/R)",
+			[]float64{2.0, 47.4, 42.0, 10.8, 60.1, 1.7, 64.0, 63.1, 1.0, 1.4, 7.9, 0.3, 3.9, 0.3, 6.7},
+			[]float64{22.6, 8.3, 44.4, 11.9, 24.6, 0.6, 5.7, 41.6, 0.0, 0.6, 6.7, 3.8, 1.0, 1.2, 1.4},
+			0.6887298747763864,
+			nil,
+		},
+		{
+			"Adjacent swap (SciPy)",
+			[]float64{0, 1, 2, 3, 4, 5},
+			[]float64{0, 1, 2, 3, 5, 4},
+			0.9428571428571429,
+			nil,
+		},
+		{
+			"Heavy ties in both (SciPy)",
+			[]float64{1, 1, 1, 2, 2, 2},
+			[]float64{1, 1, 2, 2, 3, 3},
+			0.8164965809277261,
+			nil,
+		},
+
+		// Zero standard deviation case (all same values)
+		{"Same Input Value", []float64{0, 0, 0}, []float64{0, 0, 0}, 0.00, nil},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			a, err := stats.Spearman(tc.data1, tc.data2)
+			if err != nil {
+				if err != tc.err {
+					t.Errorf("Should have returned error %s, got %s", tc.err, err)
+				}
+			} else if !veryclose(a, tc.output) {
+				t.Errorf("Result %.16f should be %.16f", a, tc.output)
+			}
+
+			// Also test the Float64Data method
+			a2, err2 := stats.Float64Data(tc.data1).Spearman(tc.data2)
+			if err2 != nil {
+				if err2 != tc.err {
+					t.Errorf("Should have returned error %s, got %s", tc.err, err2)
+				}
+			} else if !veryclose(a2, tc.output) {
+				t.Errorf("Result %.16f should be %.16f", a2, tc.output)
+			}
+		})
+	}
+}
+
+func BenchmarkSpearman(b *testing.B) {
+	s1 := []float64{2.0, 47.4, 42.0, 10.8, 60.1, 1.7, 64.0, 63.1, 1.0, 1.4, 7.9, 0.3, 3.9, 0.3, 6.7}
+	s2 := []float64{22.6, 8.3, 44.4, 11.9, 24.6, 0.6, 5.7, 41.6, 0.0, 0.6, 6.7, 3.8, 1.0, 1.2, 1.4}
+	for i := 0; i < b.N; i++ {
+		_, _ = stats.Spearman(s1, s2)
+	}
+}
+
 func ExampleAutoCorrelation() {
 	s1 := []float64{1, 2, 3, 4, 5}
 	a, _ := stats.AutoCorrelation(s1, 1)
